b4ca41288c0ebed15265d49f61c517eaae2bdefb
disapar <disaponline@yahoo.com.ar>
2021-08-31 11:14:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app  = express();
+const port = 8080;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+app.get('/about', (req,res)=>{
+    res.sendFile(__dirname + '/public/about.html')
+});
+app.get('/contact', (req,res)=>{
+    res.sendFile(__dirname + '/public/contact.html')
+})
+
+app.get('/services', (req,res)=>{
+    res.sendFile(__dirname + '/public/services.html')
+})
+app.get('*', (req,res)=>{
+    res.send('404 | Page not found')
+});
+
+app.listen(port, ()=>{
+    console.log(`Example app listen at http://localhost:${port}`)
+})
